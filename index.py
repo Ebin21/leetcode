4400c0fbe0c80921b96535b9c5efdd7f9041e0ae
Ebin21 <ebin.alex@outlook.com>
2025-11-03 08:17:23 +0530

    Leetcode updated

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ class Solution:
 
         # Iterate through the haystack
         # We only need to check up to the point where a full 'needle' can still fit.
-        # The loop runs from index 0 up to (n - m)
+        # The loop runs from index 0 up to (n - m) loops.
         for i in range(n - m + 1):
             # Check if the substring of haystack starting at index i and
             # of length m (the length of needle) is equal to needle.
